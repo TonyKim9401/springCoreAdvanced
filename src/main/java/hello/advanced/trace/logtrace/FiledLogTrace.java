e7b492a1b2c6db76a149f9fea5436f828e092b8c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-06-22 23:01:32 +0900

    JUNE.21.2023
    필드 동기화 - 개발
    
    기존 값을 유지하기 위해 begin(Sync)할때마다 파라미터로 넘기던 traceId를 LogTrace 구현체 안에 정의하여 Bean으로 정의함으로써 외부에서 호출해주는것이 아닌 trace객체의 메소드 실행시 흐름에 따라 하나의 traceId를 가지고 수행되게 함
    
    *추후 동시성 이슈가 발생할 예정임

--- src/main/java/hello/advanced/trace/logtrace/FiledLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/FiledLogTrace.java
@@ -1,38 +1,40 @@
-package hello.advanced.trace.hellotrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-public class HelloTraceV2 {
-
+public class FiledLogTrace implements LogTrace{
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
 
+    private TraceId traceIdHolder; //traceId 동기화, 동시성 이슈 발생
 
+    @Override
     public TraceStatus begin(String message) {
-        TraceId traceId = new TraceId();
+        syncTraceId();
+        TraceId traceId = traceIdHolder;
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
-    //V2 추가
-    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
-        final TraceId nextId = beforeTraceId.createNextId();
-        Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
-        return new TraceStatus(nextId, startTimeMs, message);
+    private void syncTraceId() {
+        if (traceIdHolder == null) {
+            traceIdHolder = new TraceId();
+        } else {
+            traceIdHolder = traceIdHolder.createNextId();
+        }
     }
 
+    @Override
     public void end(TraceStatus status) {
         complete(status, null);
     }
 
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
@@ -49,7 +51,17 @@ public class HelloTraceV2 {
             log.info("[{}] {}{} time={}ms ex={}",
                     traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
         }
+        releaseTraceId();
     }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null; //destroy
+        } else {
+            traceIdHolder = traceIdHolder.createPreviousId();
+        }
+    }
+
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
@@ -57,6 +69,5 @@ public class HelloTraceV2 {
         }
         return sb.toString();
     }
-
 
 }
