d88a22359281d867863fb9fa3b0642d9f6bae129
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-06-29 19:06:08 +0900

    JUNE.29.2023
    필드 동기화 - 예제 코드
    
    여러 쓰레드가 동시에 같은 인스턴스 필드 값을 변경하면서 발생하는 문제를 동시성 문제라고 한다. 스프링 빈처럼 싱글톤 객체의 필드를 변경하며 사용할 때 이러한 동시성 문제를 특히나 조심해야한다.
    
    값을 읽기만 하면 발생하지 않지만, 어디선가 값을 변경하기 때문에 발생한다.
    
    *싱글톤의 필드, static 공용 필드
    
    동시성 문제 해결 방법 -> 쓰레드 로컬 사용

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/FiledService.java
@@ -0,0 +1,28 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FiledService {
+
+    private String nameStore;
+
+    public String logic(String name) {
+        log.info("저장 name = {} -> nameStore = {}", name, nameStore);
+        nameStore = name;
+        sleep(1000); // 저장하는데 걸리는 시간 1초정도
+        log.info("조회 nameStore = {}", nameStore);
+        return nameStore;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
