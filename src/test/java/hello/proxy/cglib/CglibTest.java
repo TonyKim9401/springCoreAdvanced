5e78fd95259aede158efc026d26e02245f799fbe
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-05 14:00:39 +0900

    JULY.05.2023
    CGLIB - 예제 코드
    
    import org.springframework.cglib.proxy.MethodInterceptor; 을 써야함
    
    Enhancer : CGLIB은 Enhancer을 사용해 프록시를 생성함
    enhancer.setSuperclass(~) : 구체 클래스를 상속받아 프록시를 생성한다. 어떤 쿠체 클래스를 상속 받을지 지정한다.
    enhancer.setCallback(~) : 프록시에 적용할 실행 로직을 할당한다.
    enhancer.create() : setSuperclass 에 지정한 클래스를 상속받아 프록시를 생성한다. (부모타입)

---
+++ src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,27 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer(); // cglib 을 만드는 코드
+        enhancer.setSuperclass(ConcreteService.class); // 어떤 클래스를 기반으로 만들건지 설정, Proxy의 부모 타입 지정
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+//        Object proxy = enhancer.create();// proxy 생성
+        ConcreteService proxy = (ConcreteService) enhancer.create();// proxy 생성
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
