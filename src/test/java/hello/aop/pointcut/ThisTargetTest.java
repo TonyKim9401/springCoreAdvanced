5c671897edb3c7a44f650f015ffe174956519534
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-10 17:49:54 +0900

    JULY.10.2023
    this, target
    
    다음과 같이 적용 타입 하나를 정확하게 지정해야 한다. (패턴 표현식 사용X, 부모 타입은 허용)
    this(hello.aop.member.MemberService)
    target(hello.aop.member.MemberService)
    
    this: 스프링 Bean 객체(스프링 AOP Proxy)를 대상으로 하는 JoinPoint
    target: Target 객체(스프링 AOP Proxy 가 가리키는 실제 대상)를 대상으로 하는 JoinPoint
    
    this 사용시 interface를 기반으로 만들어진 Proxy 생성 전략에서 this(인터페이스 객체)가 아닌 this(구현 객체)로 실행할 경우, Spring Container에 등록된 Proxy Bean(인터페이스 기반)에 구현객체에 대한 정보가 없어 AOP가 적용되지 않는다.
    -> target의 경우 실제 객체가 대상이므로  인터페이스, 구현객체 상관 없이 모두 AOP가 적용된다.

---
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,63 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * application.properties
+ * spring.aop.proxy-target=true     CGLIB
+ * spring.aop.proxy-target=false    CGLIB, JDK 동적 프록시 사용
+ */
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+//@SpringBootTest(properties= "spring.aop.proxy-target-class=false")
+@SpringBootTest(properties= "spring.aop.proxy-target-class=true")
+public class ThisTargetTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy = {}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect{
+        @Around("this(hello.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberService)")
+        public Object doTargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        //interface 를 참고하여 JDK 동적 Proxy 를 생성했지만
+        //포인트컷이 구체 클래스로 되어있어서 AOP 가 동작하지 않음
+        @Around("this(hello.aop.member.MemberServiceImpl)")
+        public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberServiceImpl)")
+        public Object doTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+}
