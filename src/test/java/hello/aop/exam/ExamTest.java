2bef96b405297c5c848396f5d5ee7ee332cac9b1
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-10 18:36:52 +0900

    JULY.10.2023
    로그 출력 AOP
    
    @Trace 애노테이션을 생성하고 TraceAspect 에서 @annotation으로 @Trace에 해당하는 메소드의 정보와 인수값을 로그로 출력하는 AOP를 적용해 본다.

--- src/test/java/hello/aop/exam/ExamTest.java
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -1,11 +1,14 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(TraceAspect.class)
 @SpringBootTest
 public class ExamTest {
 
@@ -14,11 +17,10 @@ public class ExamTest {
 
     @Test
     void test() {
-
         for (int i = 0; i < 8; i++) {
             log.info("client request i = {}", i);
             examService.request("data" + i);
         }
-
     }
+
 }
